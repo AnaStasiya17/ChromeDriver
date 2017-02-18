7b0d851eeedb117707a69b9de6c9c4ab0328c903
Anastasiya17 <Anastasiyatsybulko@gmail.com>
2017-02-18 15:15:09 +0300

    Initial commit

---
+++ src/test/java/ChDriver.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by Анастасия Цыбулько on 15.02.2017.
+ */
+public class ChDriver {
+
+    @Test
+    public void chromeWebTest() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        DesiredCapabilities chrome = new DesiredCapabilities().chrome();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.findElement(By.id("text")).sendKeys("Selenium Webdriver");
+
+        driver.findElement(By.cssSelector(".suggest2-form__button")).click();
+        Thread.sleep(2000);
+        driver.get(driver.getCurrentUrl());
+        driver.findElement(By.className("needsclick")).click();
+
+
+        WebDriver augmentedDriver = new Augmenter().augment(driver);
+        File screenshot = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("screenshot.png"));
+
+
+    }
+}
